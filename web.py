902f8fe4020b6ba95cbc6fb00579df9f388189d9
lagdars47-cloud <lagdars47@gmail.com>
2026-06-06 23:54:00 +0200

    Update web.py

--- web.py
+++ web.py
@@ -69,8 +69,7 @@ if user_query := st.chat_input("Спроси что-нибудь про комп
         with st.spinner("Ищу в документах..."):
             relevant_docs = retriever.invoke(user_query)
             context_text = "\n".join([doc.page_content for doc in relevant_docs])
-
-      if use_internet:
+if use_internet:
             with st.spinner("Ищу в Википедии... 📚"):
                 try:
                     search = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
@@ -79,8 +78,7 @@ if user_query := st.chat_input("Спроси что-нибудь про комп
                 except Exception:
                     pass
         
-        chain = prompt | llm
-
+        chain = prompt | llm   
             def stream_generator():
                 for chunk in chain.stream({"context": context_text, "question": user_query}):
                     yield chunk.content
