7906efa38bb8a3d6906f0c899980048a22d0fac0
lagdars47-cloud <lagdars47@gmail.com>
2026-06-06 23:42:08 +0200

    Update web.py

--- web.py
+++ web.py
@@ -6,6 +6,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 
 st.set_page_config(page_title="Colloid AI", page_icon=":rat:")
 st.title(":rat: Корпоративный ИИ компании Colloid")
